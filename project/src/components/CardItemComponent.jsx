d6c4836ffc71f1aa560a3d16982e89fb538cff23
Hani-Alyasen <hanialyasen@gmail.com>
2025-03-17 15:21:31 +0100

    Refactor CardItemComponent to display product cards with updated layout and "View Product" button

--- project/src/components/CardItemComponent.jsx
+++ project/src/components/CardItemComponent.jsx
@@ -14,18 +14,18 @@ const CardItemComponent = () => {
     <div className="container mt-4">
       <div className="row">
         {data.map((item) => (
-          <div key={item.id} className="col-md-4 col-lg-3 mb-4">
-            <div className="card" style={{ width: "18rem" }}>
+          <div key={item.id} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4 d-flex">
+            <div className="card d-flex flex-column h-100">
               <img
                 src={item.image}
                 className="card-img-top"
                 alt={item.title}
                 style={{ height: "200px", objectFit: "contain" }}
               />
-              <div className="card-body">
+              <div className="card-body d-flex flex-column">
                 <h5 className="card-title">{item.title}</h5>
                 <p className="card-text">${item.price}</p>
-                <a href="#" className="btn btn-primary">
+                <a href="#" className="btn btn-primary mt-auto">
                   View Product
                 </a>
               </div>
