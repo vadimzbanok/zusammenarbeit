4a79ed2ebd5a831339b7461ec64d45bd0dbf2d08
ameralbuni85 <amerbraun@gmail.com>
2025-03-17 13:25:39 +0100

    footer

--- project/src/pages/HomePage.jsx
+++ project/src/pages/HomePage.jsx
@@ -1,6 +1,11 @@
+import FooterComponent from "../components/FooterComponent"
+
 const HomePage = () => {
   return (
+    <>
     <div className="text-[red]">HomePage</div>
+    {<FooterComponent />}
+    </>
   )
 }
 export default HomePage
