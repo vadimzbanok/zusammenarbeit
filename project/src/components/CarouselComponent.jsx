93862ada1d7c277105dcb97d18a0a90559862adb
Hani-Alyasen <hanialyasen@gmail.com>
2025-03-18 14:44:43 +0100

    Refactor CarouselComponent to add new images and improve animation

--- project/src/components/CarouselComponent.jsx
+++ project/src/components/CarouselComponent.jsx
@@ -6,7 +6,12 @@ const productData = [
   { id: 2, image: "./images/carousel/2.jpg" },
   { id: 3, image: "./images/carousel/3.jpg" },
   { id: 4, image: "./images/carousel/4.jpg" },
-  { id: 5, image: "./images/carousel/5.jpg" }
+  { id: 5, image: "./images/carousel/5.jpg" },
+  { id: 6, image: "./images/carousel/6.jpg" },
+  { id: 7, image: "./images/carousel/7.jpg" },
+  { id: 8, image: "./images/carousel/8.jpg" },
+  { id: 9, image: "./images/carousel/9.jpg" },
+  { id: 10, image: "./images/carousel/10.jpg" },
 ];
 
 const CarouselComponent = () => {
@@ -29,23 +34,28 @@ const CarouselComponent = () => {
     <div className="carousel-container">
       <div className="carousel">
         <AnimatePresence mode="wait">
-          {productData.map((item, index) => (
-            index === currentIndex && (
-              <motion.div
-                key={item.id}
-                initial={{ opacity: 0, scale: 0.8, x: 100 }}
-                animate={{ opacity: 1, scale: 1, x: 0 }}
-                exit={{ opacity: 0, scale: 0.8, x: -100 }}
-                transition={{ duration: 0.5, ease: "easeInOut" }}
-               
-              >
-                <img src={item.image} alt={`Slide ${index + 1}`} className="carousel-img" onError={(e) => e.target.src = "./images/placeholder.jpg"} />
-              </motion.div>
-            )
-          ))}
+          {productData.map(
+            (item, index) =>
+              index === currentIndex && (
+                <motion.div
+                  key={item.id}
+                  initial={{ opacity: 0, scale: 0.8, x: 100 }}
+                  animate={{ opacity: 1, scale: 1, x: 0 }}
+                  exit={{ opacity: 0, scale: 0.8, x: -100 }}
+                  transition={{ duration: 0.5, ease: "easeInOut" }}
+                >
+                  <img
+                    src={item.image}
+                    alt={`Slide ${index + 1}`}
+                    className="carousel-img"
+                    onError={(e) => (e.target.src = "./images/placeholder.jpg")}
+                  />
+                </motion.div>
+              )
+          )}
         </AnimatePresence>
       </div>
-      
+
       <style>{`
         .carousel-container {
           display: flex;
