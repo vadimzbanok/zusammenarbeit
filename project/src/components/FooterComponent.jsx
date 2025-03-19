782911d10c8c242147655a15c582d820986b7edd
vadimzbanok <vadimzbanok@gmail.com>
2025-03-19 10:10:34 +0100

    fix: clean up whitespace in App.css and FooterComponent.jsx

--- project/src/components/FooterComponent.jsx
+++ project/src/components/FooterComponent.jsx
@@ -43,6 +43,7 @@ const FooterComponent = () => {
           </a>
         </div>
       </div>
+      
 
       <div className="bg-gray-100 pt-2">
         <div className="flex pb-5 px-3 m-auto pt-5 border-t text-gray-800 text-sm flex-col max-w-screen-lg items-center">
