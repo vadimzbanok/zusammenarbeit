e7ecddc752af7c01071774e64b333c60071b213f
vadimzbanok <vadimzbanok@gmail.com>
2025-03-19 15:43:17 +0100

    update

--- project/src/components/FooterComponent.jsx
+++ project/src/components/FooterComponent.jsx
@@ -3,118 +3,43 @@ import logo2 from '/logo2.svg';
 
 const FooterComponent = () => {
   return (
-    <div className="bg-gray-100">
-      <div className="max-w-screen-lg px-4 sm:px-6 text-gray-800 sm:grid md:grid-cols-4 sm:grid-cols-2 mx-auto">
-        <div className="p-5">
-          <img className="logo" src={logo2} alt="logo2" />
+    <footer className="bg-gray-100 text-gray-800 text-center">
+      <div className="max-w-screen-lg mx-auto px-6 py-8 grid md:grid-cols-3 sm:grid-cols-1 gap-6 text-center">
+        <div className="flex justify-center">
+          <img className="w-32" src={logo2} alt="logo2" />
         </div>
-        <div className="p-5">
+        
+        <div>
+          <h3 className="text-indigo-600 font-bold uppercase text-sm mb-2">Policies</h3>
+          <Link to="/privacy" className="block hover:text-indigo-600 no-underline">Privacy Policy</Link>
+          <a href="/#" className="block hover:text-indigo-600 no-underline">Conditions</a>
         </div>
-        <div className="p-5">
-          <Link to={"/privacy"} className="my-3 block no-underline" >
-            Privacy Policy <span className="text-teal-600 text-xs p-1"></span>
-          </Link>
-          <a className="my-3 block no-underline" href="/#">
-            Conditions <span className="text-teal-600 text-xs p-1"></span>
-          </a>
-        </div>
-        <div className="p-5">
-          <div className="text-sm uppercase text-indigo-600 font-bold">Contact us</div>
-          <a className="my-3 block no-underline" href="/#">
-            Berlin, Hauptstraße 25, DCI Quarters
-            <span className="text-teal-600 text-xs p-1"></span>
-          </a>
-          <a className="my-3 block no-underline" href="/#">
-            contact@shopify.com
-            <span className="text-teal-600 text-xs p-1"></span>
-          </a>
+        
+        <div>
+          <h3 className="text-indigo-600 font-bold uppercase text-sm mb-2">Contact Us</h3>
+          <p>Berlin, Hauptstraße 25, DCI Quarters</p>
+          <p>contact@shopify.com</p>
         </div>
       </div>
       
-
-      <div className="bg-gray-100 pt-2">
-        <div className="flex  px-3 pt-5 border-t text-gray-800 text-sm flex-col items-center">
-          <div className="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
-            <a href="https://x.com/?lang=en" target="_blank" rel="noopener noreferrer" className="w-6 mx-1">
-              <svg
-                className="fill-current cursor-pointer text-gray-500 hover:text-indigo-600"
-                width="100%"
-                height="100%"
-                viewBox="0 0 24 24"
-                version="1.1"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-                xmlSpace="preserve"
-                style={{
-                  fillRule: 'evenodd',
-                  clipRule: 'evenodd',
-                  strokeLinejoin: 'round',
-                  strokeMiterlimit: 2,
-                }}
-              >
-                <path
-                  id="Twitter"
-                  d="M24,12c0,6.627 -5.373,12 -12,12c-6.627,0 -12,-5.373 -12,-12c0,-6.627 5.373,-12 12,-12c6.627,0 12,5.373 12,12Zm-6.465,-3.192c-0.379,0.168 -0.786,0.281 -1.213,0.333c0.436,-0.262 0.771,-0.676 0.929,-1.169c-0.408,0.242 -0.86,0.418 -1.341,0.513c-0.385,-0.411 -0.934,-0.667 -1.541,-0.667c-1.167,0 -2.112,0.945 -2.112,2.111c0,0.166 0.018,0.327 0.054,0.482c-1.754,-0.088 -3.31,-0.929 -4.352,-2.206c-0.181,0.311 -0.286,0.674 -0.286,1.061c0,0.733 0.373,1.379 0.94,1.757c-0.346,-0.01 -0.672,-0.106 -0.956,-0.264c-0.001,0.009 -0.001,0.018 -0.001,0.027c0,1.023 0.728,1.877 1.694,2.07c-0.177,0.049 -0.364,0.075 -0.556,0.075c-0.137,0 -0.269,-0.014 -0.397,-0.038c0.268,0.838 1.048,1.449 1.972,1.466c-0.723,0.566 -1.633,0.904 -2.622,0.904c-0.171,0 -0.339,-0.01 -0.504,-0.03c0.934,0.599 2.044,0.949 3.237,0.949c3.883,0 6.007,-3.217 6.007,-6.008c0,-0.091 -0.002,-0.183 -0.006,-0.273c0.413,-0.298 0.771,-0.67 1.054,-1.093Z"
-                ></path>
-              </svg>
+      <div className="border-t pt-4">
+        <div className="flex flex-col items-center space-y-4 max-w-screen-lg mx-auto px-6">
+          <p className="text-sm">© Copyright 2020. All Rights Reserved.</p>
+          <div className="flex space-x-4 justify-center">
+            <a href="https://x.com/?lang=en" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-indigo-600">
+              <i className="fab fa-twitter text-xl"></i>
             </a>
-           
-            <a href="https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2F" target="_blank" rel="noopener noreferrer" className="w-6 mx-1">
-              <svg
-                className="fill-current cursor-pointer text-gray-500 hover:text-indigo-600"
-                width="100%"
-                height="100%"
-                viewBox="0 0 24 24"
-                version="1.1"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-                xmlSpace="preserve"
-                style={{
-                  fillRule: 'evenodd',
-                  clipRule: 'evenodd',
-                  strokeLinejoin: 'round',
-                  strokeMiterlimit: 2,
-                }}
-              >
-                <path
-                  id="Facebook"
-                  d="M24,12c0,6.627 -5.373,12 -12,12c-6.627,0 -12,-5.373 -12,-12c0,-6.627 5.373,-12 12,-12c6.627,0 12,5.373 12,12Zm-11.278,0l1.294,0l0.172,-1.617l-1.466,0l0.002,-0.808c0,-0.422 0.04,-0.648 0.646,-0.648l0.809,0l0,-1.616l-1.295,0c-1.555,0 -2.103,0.784 -2.103,2.102l0,0.97l-0.969,0l0,1.617l0.969,0l0,4.689l1.941,0l0,-4.689Z"
-                ></path>
-              </svg>
+            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-indigo-600">
+              <i className="fab fa-facebook text-xl"></i>
             </a>
-            <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer" className="w-6 mx-1">
-              <svg
-                className="fill-current cursor-pointer text-gray-500 hover:text-indigo-600"
-                width="100%"
-                height="100%"
-                viewBox="0 0 24 24"
-                version="1.1"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-                xmlSpace="preserve"
-                style={{
-                  fillRule: 'evenodd',
-                  clipRule: 'evenodd',
-                  strokeLinejoin: 'round',
-                  strokeMiterlimit: 2,
-                }}
-              >
-                <g id="Layer_1">
-                  <circle id="Oval" cx="12" cy="12" r="12"></circle>
-                  <path
-                    id="Shape"
-                    d="M19.05,8.362c0,-0.062 0,-0.125 -0.063,-0.187l0,-0.063c-0.187,-0.562 -0.687,-0.937 -1.312,-0.937l0.125,0c0,0 -2.438,-0.375 -5.75,-0.375c-3.25,0 -5.75,0.375 -5.75,0.375l0.125,0c-0.625,0 -1.125,0.375 -1.313,0.937l0,0.063c0,0.062 0,0.125 -0.062,0.187c-0.063,0.625 -0.25,1.938 -0.25,3.438c0,1.5 0.187,2.812 0.25,3.437c0,0.063 0,0.125 0.062,0.188l0,0.062c0.188,0.563 0.688,0.938 1.313,0.938l-0.125,0c0,0 2.437,0.375 5.75,0.375c3.25,0 5.75,-0.375 5.75,-0.375l-0.125,0c0.625,0 1.125,-0.375 1.312,-0.938l0,-0.062c0,-0.063 0,-0.125 0.063,-0.188c0.062,-0.625 0.25,-1.937 0.25,-3.437c0,-1.5 -0.125,-2.813 -0.25,-3.438Zm-4.634,3.927l-3.201,2.315c-0.068,0.068 -0.137,0.068 -0.205,0.068c-0.068,0 -0.136,0 -0.204,-0.068c-0.136,-0.068 -0.204,-0.204 -0.204,-0.34l0,-4.631c0,-0.136 0.068,-0.273 0.204,-0.341c0.136,-0.068 0.272,-0.068 0.409,0l3.201,2.316c0.068,0.068 0.136,0.204 0.136,0.34c0.068,0.136 0,0.273 -0.136,0.341Z"
-                    style={{ fill: 'rgb(255, 255, 255)' }}
-                  ></path>
-                </g>
-              </svg>
+            <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-indigo-600">
+              <i className="fab fa-youtube text-xl"></i>
             </a>
-            
           </div>
-          <div className="m-4">© Copyright 2020. All Rights Reserved.</div>
         </div>
       </div>
-    </div>
-  )
-}
-export default FooterComponent
+    </footer>
+  );
+};
+
+export default FooterComponent;
