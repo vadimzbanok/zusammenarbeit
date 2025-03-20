b00943d1eec4d048932366a7f5784f26fe2be737
vadimzbanok <vadimzbanok@gmail.com>
2025-03-20 09:40:03 +0100

    notfound page

--- project/src/pages/NotFoundPage.jsx
+++ project/src/pages/NotFoundPage.jsx
@@ -1,6 +1,25 @@
-const NotFoundPage = () => {
+import React from "react";
+import { useNavigate,Link } from "react-router-dom";
+
+function ErrorPage() {
+  const navigate = useNavigate()
+
   return (
-    <div>NotFoundPage</div>
-  )
+    <div id="notfound">
+      <div className="notfound">
+        <div className="notfound-404">
+          <h3>Oops!</h3>
+          <h1>
+            <span>#</span>
+            <span>4</span>
+            <span>0</span>
+            <span>4</span>
+          </h1>
+        </div>
+        <h2>Sorry, the page you requested was not found</h2>
+        <Link to="/" className="notfound-button no-underline">GO BACK</Link>
+      </div>
+    </div>
+  );
 }
-export default NotFoundPage
+export default ErrorPage;
